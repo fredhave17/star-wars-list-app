9f7155cc3516bf3aa7b2d62f6c98535f5e02e075
Frederico Filho <fredhave17@gmail.com>
2024-06-26 00:56:42 -0300

    feat: created just screen components

---
+++ StarWarsList/src/screen/DetailScreen/DetailScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "../../components/Text/Text";
+
+export const DetailScreen = () => {
+  return (
+    <View>
+      <Text>Detail Screen</Text>
+    </View>
+  );
+};
