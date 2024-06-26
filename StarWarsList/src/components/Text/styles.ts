acb142eb0ed2af3bf1e310c020f3a03f889f7f84
Frederico Filho <fredhave17@gmail.com>
2024-06-26 00:24:57 -0300

    feat: created text component

---
+++ StarWarsList/src/components/Text/styles.ts
@@ -0,0 +1,66 @@
+import { TextStyle, ViewStyle } from "react-native";
+import { $COLORS } from "src/theme/colors";
+
+export type TextVariants =
+  | "headingLarge"
+  | "headingLargeBold"
+  | "headingMedium"
+  | "headingMediumBold"
+  | "paragraphLarge"
+  | "paragraphMedium";
+
+export const $fontSizes: Record<TextVariants, TextStyle> = {
+  headingLarge: {
+    fontSize: 20,
+    lineHeight: 28,
+    color: $COLORS.colorText,
+  },
+  headingLargeBold: {
+    fontSize: 20,
+    lineHeight: 28,
+    color: $COLORS.colorText,
+    fontWeight: "bold",
+  },
+  headingMedium: {
+    fontSize: 16,
+    lineHeight: 22.4,
+    color: $COLORS.colorText,
+  },
+  headingMediumBold: {
+    fontSize: 16,
+    lineHeight: 22.4,
+    color: $COLORS.colorText,
+    fontWeight: "bold",
+  },
+  paragraphLarge: {
+    fontSize: 14,
+    lineHeight: 19.6,
+    color: $COLORS.colorText,
+  },
+  paragraphMedium: {
+    fontSize: 12,
+    lineHeight: 16.8,
+    color: $COLORS.colorText,
+  },
+};
+
+export const styles = {
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  } as ViewStyle,
+
+  headingLarge: {
+    ...$fontSizes.headingLarge,
+    color: "#333",
+    // Outros estilos específicos para headingLarge
+  } as TextStyle,
+
+  paragraph: {
+    ...$fontSizes.paragraphMedium,
+    color: "#666",
+    // Outros estilos específicos para paragraphMedium
+  } as TextStyle,
+};
